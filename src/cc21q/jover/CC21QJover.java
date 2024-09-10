db1ebd6c13c5a529fe6130bec4b41f80d5e5516e
BlackCoder23 <jpj7176@students.uc-bcf.edu.ph>
2024-09-10 14:03:40 +0800

    Add files via upload

---
+++ src/cc21q/jover/CC21QJover.java
@@ -0,0 +1,12 @@
+package cc21q.jover;
+
+public class CC21QJover {
+ public static void main(String[] args) {
+        // Declare and initialize the variables
+        String name = "Jhann Renzol Jover";
+        int age = 25;
+
+        // Print the output
+        System.out.println("Hello I'm " + name + " and I'm " + age + " years old");
+    }
+}
